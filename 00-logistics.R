b0cbe69181e18be290b2a8c82dfa6f144aa73a94
Rintu Kutum <rintukutum@gmail.com>
2022-06-27 12:54:49 +0530

    added the 00-logistics

---
+++ 00-logistics.R
@@ -0,0 +1,42 @@
+#' --------------------------
+#' DAY 1
+#' SESSION - MORNING HANDS-ON
+#' --------------------------
+#' 
+#' INTRODUCTION TO R
+#' 
+#' Live walk through
+#' 
+#' https://www.r-project.org
+#' 
+#' Brief overview
+#' https://www.r-project.org/about.html
+#' 
+#' 
+#' 
+#' ------------------
+#' Books related to R
+#' ------------------
+#' https://www.r-project.org/doc/bib/R-jabref.html
+#' https://www.r-project.org/doc/bib/R-books.html
+#' 
+#' ----------------
+#' Useful Resources
+#' ----------------
+#' https://www.r-project.org/other-docs.html
+#' 
+#' Ecology & Epidemiology 
+#' Statistical Thinking
+#' Biomedical Statistics
+#' Time Series
+#' Multivariate Analysis
+#' 
+#' ----- * -----
+#' INSTALL R
+#' https://cloud.r-project.org
+#' 
+#' ----- * -----
+#' INSTALL RStudio
+#' https://www.rstudio.com/products/rstudio/
+#' 
+#' 
